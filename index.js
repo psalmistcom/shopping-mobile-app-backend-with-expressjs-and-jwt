b52c72e09962e0946b421e635eec6b7f17bf154b
psalmistcom <dadewale9@gmail.com>
2023-12-21 23:04:29 +0100

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const port = 3000;
+
+dotenv.config();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
