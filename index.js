03463ed8cfabda5082f3997e24b2f0d3d41286eb
psalmistcom <dadewale9@gmail.com>
2023-12-21 23:44:10 +0100

    models drafted

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const port = 3000;
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT || port, () =>
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
